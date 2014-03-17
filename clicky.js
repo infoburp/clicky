723a421200145885aa93cc1da2ff46ccaaeb25f4
Graeme Wolfendale <gwolfendale@gmail.com>
2014-03-17 19:30:03 +0000

    Update clicky.js

--- clicky.js
+++ clicky.js
@@ -23,10 +23,7 @@ function buyAuto(){
 		autofactor += .8;
 		auto *= 2;
 		updatescreen();
-	} else {
-		clicks += manual;
-		updatescreen();
-	}
+	} 
 }
 function upgradeBomb(){
 	if (clicks > bombupgradecost) {
